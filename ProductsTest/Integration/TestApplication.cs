abf31bccace0e79767cde65711110fa800dd076d
Alberto <alberto@ribao.dev>
2024-08-30 17:50:47 +0100

    Second commit

--- ProductsTest/Integration/TestApplication.cs
+++ ProductsTest/Integration/TestApplication.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ProductsWebAPI.Data;
 
 namespace ProductsTest.Integration;
 
@@ -11,16 +8,6 @@ internal class TestApplication(string environment = "Development") : WebApplicat
     protected override IHost CreateHost(IHostBuilder builder)
     {
         builder.UseEnvironment(environment);
-
-        // Add mock/test services to the builder here
-        builder.ConfigureServices(services =>
-        {
-            services.AddScoped(sp => new DbContextOptionsBuilder<ProductsDbContext>()
-                .UseInMemoryDatabase("Tests")
-                .UseApplicationServiceProvider(sp)
-                .Options);
-        });
-
         return base.CreateHost(builder);
     }
 }
